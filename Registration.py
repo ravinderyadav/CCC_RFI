0288ab3de5e1146e970ec745d8331266eb825b40
dpsharma15 <42308574+dpsharma15@users.noreply.github.com>
2024-05-12 15:25:58 +0300

    Update Registration.py

--- Registration.py
+++ Registration.py
@@ -17,7 +17,7 @@ warnings.filterwarnings('ignore')
 def app():
     # List of image paths (replace with your own image paths)
     image_paths = [
-        "https://github.com/dpsharma15/CCC_RFI/blob/main/img-4.png"
+        "https://raw.githubusercontent.com/dpsharma15/CCC_RFI/main/img-4.png"
     ]
 
     # Create a placeholder for the image

3897051eeffcda7467e9839505dee8c6a54b8aab
dpsharma15 <42308574+dpsharma15@users.noreply.github.com>
2024-07-18 14:15:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,25 +18,31 @@ class MultiApp:
 
     def run(self):
         st.sidebar.markdown("<h1 style='color: orange; font-weight: bold;'>CCC_RFI</h1>", unsafe_allow_html=True)
-        app_titles = ["Select a page"] + [app_dict["title"] for app_dict in self.apps]
-        app_choice = st.sidebar.selectbox("Select a page:", app_titles)
+        app_titles = ["Select a Programme"] + [app_dict["title"] for app_dict in self.apps]
+        
+        # Initialize session state for selected_app if it doesn't exist
+        if 'selected_app' not in st.session_state:
+            st.session_state.selected_app = "Select a Programme"
+        
+        # Update session state based on the user's selection
+        st.session_state.selected_app = st.sidebar.selectbox("Select a Programme:", app_titles, index=0)
 
-        if app_choice == "Select a page":
+        if st.session_state.selected_app == "Select a Programme":
             st.markdown(
-                "<div style='display: flex; justify-content: center; align-items: center; height: 80vh;'><h2 style='color: red;'>Please select a program from the dropdown list.</h2></div>",
+                "<div style='display: flex; justify-content: center; align-items: center; height: 80vh;'><h2 style='color: red;'>Please select a programme from the dropdown list.</h2></div>",
                 unsafe_allow_html=True,
             )
         else:
             for app_dict in self.apps:
-                if app_choice == app_dict["title"]:
+                if st.session_state.selected_app == app_dict["title"]:
                     app_dict["function"]()
 
 # Create an instance of MultiApp
 multi_app = MultiApp()
 
 # Add apps to the MultiApp instance
-multi_app.add_app("Registration", Registration.app)
-multi_app.add_app("Inspection_result", Inspection_result.app)
+multi_app.add_app("Registration Programme", Registration.app)
+multi_app.add_app("Inspection Result Programme", Inspection_result.app)
 
 # Run the MultiApp instance
 multi_app.run()
